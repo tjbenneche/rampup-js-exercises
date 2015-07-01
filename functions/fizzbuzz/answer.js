d834e2832e24535a4d7461530279dce74454d624
Tom Benneche <tom@tjbenneche.com>
2015-06-30 23:42:03 -0500

    moved fizzbuzz and added answer

--- functions/fizzbuzz/answer.js
+++ functions/fizzbuzz/answer.js
@@ -15,7 +15,7 @@ while (_keepLooping) {
 		}
 		else if ((_dayOfSummer % 7) === 0) {
 			alert("Brent will be wearing a Pink Shirt on that day");
-		}	
+		}
 		else {
 			alert("We do not know what Brent will be wearing that day");
 		}
@@ -23,10 +23,31 @@ while (_keepLooping) {
 	}
     else if ((_dayOfSummer < 0) || (_dayOfSummer > 365)){
         alert("You must enter an Integer greater than 0 and less than 365");
-		_dayOfSummer = prompt("Input the Day of the Summer");  
+		_dayOfSummer = prompt("Input the Day of the Summer");
     }
 	else {
 		alert("Sorry, you did not enter an Integer for the Day of the Summer");
 		_dayOfSummer = prompt("Input the Day of the Summer");
 	}
 }
+
+for (var i=1; i <= 95; i++) {
+		if ((i % 21) === 0) {
+			_blueTank++;
+		}
+		else if ((i % 3) === 0) {
+			_yellowShirt++;
+		}
+		else if ((i % 7) === 0) {
+			_pinkShirt++;
+		}
+		else {
+			_unknownTop++;
+		}
+
+}
+
+alert("Brent wore a Yellow Shirt " + _yellowShirt + " times this summer.")
+alert("Brent wore a Pink Shirt " + _pinkShirt + " times this summer.")
+alert("Brent wore a Blue Tank " + _blueTank + " times this summer.")
+alert("Brent wore an Unknown Top " + _unknownTop + " times this summer.")
---
+++ functions/fizzbuzz/problem.js
@@ -0,0 +1,18 @@
+/*FizzBuzz
+
+Brent is extremely particular about his outfits. Starting with the 1st day
+of summer, he wears a yellow shirt every 3rd day and a pink shirt every
+7th day. On conflicting days (i.e. days that are multiples of both 3 and 7),
+he opts for his favorite blue tank instead.
+
+1) Ask the user to input the day of summer as an integer, and store it in a
+variable. Use a conditional to test for the various repsonses they could give,
+and alert what Brent will be wearing that day. Keep in mind a user may not
+input a number, and make sure your code is able to handle that with an
+appropriate response!
+
+
+2) Use a loop to calculate the number of times each summer Brent will wear
+his yellow shirt, pink shirt, and blue tank. Store each in a variable and
+alert them.
+*/
