618cb5c76d62ab99b578ce18893b0f1c0e789a84
Tom Benneche <tom@tjbenneche.com>
2015-07-20 17:05:55 -0500

    week 6 exercises added

--- week6/main.js
+++ week6/main.js
@@ -1,1 +1,10 @@
-test
+// stop propagation
+
+$('div').click(function(e){
+  e.stopPropagation();
+  $(this).toggleClass('selected');
+})
+
+$('#ancestors').click(function(){
+  $(this).parents().css('border-color', 'red');
+});
